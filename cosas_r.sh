ea14cb465a72eb5af7a00b6385cfc1a1fe43f584
Alejandro Acevedo, PhD <30531365+alejandroacevedo2305@users.noreply.github.com>
2023-05-29 18:41:48 -0400

    Delete cosas_r.sh

--- cosas_r.sh
+++
@@ -1,6 +0,0 @@
-mamba install -c conda-forge r-base -y
-mamba install -c r r-tidyverse -y
-mamba install -c bioconda bioconductor-biocinstaller -y
-mamba install -c bioconda bioconductor-clusterprofiler -y
-mamba install -c bioconda bioconductor-reactomepa -y
-mamba install -c conda-forge r-stringi -y
